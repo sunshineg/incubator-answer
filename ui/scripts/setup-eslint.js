9d6b42da80689e454b5990ca225dfc6373240c16
LinkinStars <linkinstar@foxmail.com>
2025-03-17 15:59:10 +0800

    docs(ui): add Apache license header to setup-eslint.js script

--- ui/scripts/setup-eslint.js
+++ ui/scripts/setup-eslint.js
@@ -1,4 +1,21 @@
-#!/usr/bin/env node
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 const fs = require('fs');
 const path = require('path');
@@ -9,7 +26,7 @@ const ROOT_DIR = path.resolve(UI_DIR, '..');   // Project root directory
 const GIT_DIR = getGitDir(ROOT_DIR);           // Git root directory
 const HUSKY_DIR = path.join(GIT_DIR, '.husky');
 
-// 查找 Git 目录
+// Find Git directory
 function getGitDir(startDir) {
   let currentDir = startDir;
 
